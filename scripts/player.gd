330551f578f5eef914f55f191490995a2637eefc
ryzbaka <hamzaali151214@hotmail.com>
2024-07-29 20:58:14 +0530

    input hold based jump height

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,12 +10,17 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
+		
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
+		
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if not is_on_floor() && Input.is_action_pressed("ui_accept"): # input hold based jump height???
+		velocity.y -= 7
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
