c349eb706ec1c42894f5c106e9c95364f7ae783f
ryzbaka <hamzaali151214@hotmail.com>
2024-07-29 21:06:23 +0530

    tighter movement i guess

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -12,7 +12,7 @@ func _physics_process(delta):
 	# Add the gravity.
 		
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * delta * 1.5
 	
 		
 	
@@ -20,13 +20,15 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	if not is_on_floor() && Input.is_action_pressed("ui_accept"): # input hold based jump height???
-		velocity.y -= 7
+		velocity.y -= 15
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if Input.is_action_pressed("ui_accept"):
+			velocity.x *=1.5
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
